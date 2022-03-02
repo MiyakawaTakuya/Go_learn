f20cb469fed189671ccb847f989e999333f2d7b8
Miyakawa Takuya <omyakawa@gmail.com>
2022-03-02 16:35:08 +0900

    up date Pai default.go

--- pkg/pai/default.go
+++ pkg/pai/default.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"reflect"
+	"sort"
 	"strconv"
 	"strings"
 	// "math"
@@ -50,4 +52,75 @@ func GetStr() (str string) {
 	s, _ := reader.ReadString('\n') //改行が現れるまでのstringを読み込む
 	str = strings.TrimSpace(s)      //余分なものを取り除いてあげる 改行
 	return
+}
+
+//スライスから特定の要素を削除する関数
+func Remove_str(strings []string, search string) []string {
+	result := []string{}
+	for _, v := range strings {
+		if v != search {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+//スライスから特定の要素を削除する関数 int
+func Remove_int(ints []int, search int) []int {
+	result := []int{}
+	for _, v := range ints {
+		if v != search {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+//スライスから特定の要素を含んでいるかどうかを返す関数
+func Contains(list interface{}, target interface{}) (bool, error) {
+
+	switch list.(type) {
+	default:
+		return false, fmt.Errorf("%v is an unsupported type", reflect.TypeOf(list))
+	case []int:
+		revert := list.([]int)
+		for _, r := range revert {
+			if target == r {
+				return true, nil
+			}
+		}
+		return false, nil
+
+	case []uint64:
+		revert := list.([]uint64)
+		for _, r := range revert {
+			if target == r {
+				return true, nil
+			}
+		}
+		return false, nil
+
+	case []string:
+		revert := list.([]string)
+		for _, r := range revert {
+			if target == r {
+				return true, nil
+			}
+		}
+		return false, nil
+	}
+
+	return false, fmt.Errorf("processing failed")
+}
+
+//スライスの最大値を取得
+func MaxInt(a []int) int {
+	sort.Sort(sort.IntSlice(a))
+	return a[len(a)-1]
+}
+
+//スライスの最小値を取得
+func MinInt(a []int) int {
+	sort.Sort(sort.IntSlice(a))
+	return a[0]
 }
