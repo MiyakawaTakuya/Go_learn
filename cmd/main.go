dbddfad4458bc5cc30007bfa0e4768e09d871ee6
Miyakawa Takuya <omyakawa@gmail.com>
2022-03-04 20:53:29 +0900

    on the way section9

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"Go_learn/pkg/pai"
+	// "Go_learn/pkg/pai"
+	"Go_learn/pkg/udemy"
 )
 
 // var sc = bufio.NewScanner(os.Stdin)
 
 func main() {
 	// pai.C052()
-	pai.C053()
+	// pai.C053()
+
+	udemy.Section9()
 
 }
-
--- pkg/udemy/section9.go
+++ pkg/udemy/section9.go
@@ -54,4 +54,38 @@ func Section9() {
 	fmt.Println(len(sl3_)) //12
 	fmt.Println(cap(sl3_)) //20  容量が足りなくなると自動的に倍になる仕様になっている
 
+	////スライスをコピーするとき
+	sl10 := []int{1, 2, 3, 4, 5}
+	sl11 := make([]int, 5, 10)
+	fmt.Println(sl11) //[0 0 0 0 0]
+	n := copy(sl11, sl10)
+	fmt.Println(n, sl11)  //5 [1 2 3 4 5]
+	fmt.Printf("%T\n", n) //int 変数nはコピーに成功した数が返ってきている
+
+	//for スライス
+	sl12 := []string{"A", "B", "C"}
+	fmt.Println(sl12)
+	for i := range sl12 {
+		fmt.Println(i, sl12[i])
+	}
+	//古典的な書き方
+	for i := 0; i < len(sl12); i++ {
+		fmt.Println(sl12[i])
+	}
+
+	//スライス 可変長引数
+	fmt.Println(sum(1,3,5))
+	fmt.Println(sum(1,3,5,123,543,656564))
+    sl13 := []int{1,2,4,6,8}
+	fmt.Println(sum(sl13...))
+
+}
+
+//引数に渡された複数の整数の合計値を返す関数
+func sum(s ...int) int{
+	n := 0
+	for _,v := range s{
+		n += v
+	}
+	return n
 }
